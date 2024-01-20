8d61e998c540e7d7dc2dd5ccfe313337709365e1
sirvan monfared <sirvan.monfared.dev@gmail.com>
2024-01-20 22:32:58 +0330

    s01e12

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+        body {
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            font-family: sans-serif;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+    $books = [
+        [
+            "title" => "Harry Potter and the sorcerer's Stone",
+            "author" => "J.K Rolling",
+            "releaseYear" => 1998,
+            "genres" => "Novel, Children's literature, Fantasy Fiction, High fantasy",
+            "characters" => "Harry Potter, Hermione Granger, Lord Voldemort, Albus Dambledor"
+        ],
+        [
+            "title" => "Harry Potter and the Chamber of Secrets",
+            "author" => "J.K Rolling",
+            "releaseYear" => 1997,
+            "genres" => "Novel, Children's literature, Fantasy Fiction, High fantasy",
+            "characters" => "Harry Potter, Hermione Granger, Lord Voldemort, Albus Dambledor"
+        ],
+        [
+            "title" => "Harry Potter and Prisoner of Azkaban",
+            "author" => "J.K Rolling",
+            "releaseYear" => 1999,
+            "genres" => "Novel, Children's literature, Fantasy Fiction, High fantasy",
+            "characters" => "Harry Potter, Hermione Granger, Lord Voldemort, Albus Dambledor"
+        ],
+        [
+            "title" => "Harry Potter and the Goblet Of Fire",
+            "author" => "J.K Rolling",
+            "releaseYear" => 2000,
+            "genres" => "Novel, Children's literature, Fantasy Fiction, High fantasy",
+            "characters" => "Harry Potter, Hermione Granger, Lord Voldemort, Albus Dambledor"
+        ],
+        [
+            "title" => "Harry Potter and the Order Of Pheonix",
+            "author" => "J.K Rolling",
+            "releaseYear" => 2003,
+            "genres" => "Novel, Children's literature, Fantasy Fiction, High fantasy",
+            "characters" => "Harry Potter, Hermione Granger, Lord Voldemort, Albus Dambledor"
+        ],
+        [
+            "title" => "Harry Potter and the Half-blood Prince",
+            "author" => "J.K Rolling",
+            "releaseYear" => 2005,
+            "genres" => "Novel, Children's literature, Fantasy Fiction, High fantasy",
+            "characters" => "Harry Potter, Hermione Granger, Lord Voldemort, Albus Dambledor"
+        ],
+        [
+            "title" => "Harry Potter and the Deathly Hollows",
+            "author" => "J.K Rolling",
+            "releaseYear" => 2007,
+            "genres" => "Novel, Children's literature, Fantasy Fiction, High fantasy",
+            "characters" => "Harry Potter, Hermione Granger, Lord Voldemort, Albus Dambledor"
+        ]
+    ];
+    ?>
+
+    <?php foreach ($books as $book) : ?>
+        <ul>
+            <li><strong>title: </strong><?= $book['title'] ?></li>
+            <li><strong>author:</strong> <?= $book['author'] ?></li>
+            <li><strong>release year:</strong> <?= $book['releaseYear'] ?></li>
+            <li><strong>genres:</strong> <?= $book['genres'] ?></li>
+            <li><strong>characters:</strong> <?= $book['characters'] ?></li>
+        </ul>
+    <?php endforeach; ?>
+
+
+</body>
+
+</html>
