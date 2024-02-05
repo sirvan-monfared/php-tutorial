04105ea32316e91134ab8961e5024f574426184f
sirvan monfared <sirvan.monfared.dev@gmail.com>
2024-02-05 18:22:53 +0330

    s04e06

--- database.php
+++ database.php
@@ -1,4 +1,10 @@
 <?php
-$connection = mysqli_connect('localhost', 'root', '123', 'tutorial_php');
+// $connection = mysqli_connect('localhost', 'root', '123', 'tutorial_php');
 
-mysqli_set_charset($connection, 'utf8mb4');
+// mysqli_set_charset($connection, 'utf8mb4');
+
+$dsn = "mysql:host=localhost;dbname=tutorial_php;charset=utf8mb4";
+
+$pdo = new PDO($dsn, 'root', '123');
+
+
--- index.php
+++ index.php
@@ -4,4 +4,10 @@ define('PROJECT_URL', '/php-tutorial/');
 require("./database.php");
 require("./functions.php");
 
+$statement = $pdo->prepare("SELECT * FROM `posts`");
+$statement->execute();
+
+$posts = $statement->fetch(PDO::FETCH_ASSOC);
+
+dd($posts);
 require("router.php");
