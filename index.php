78bad75fae81807b2180f1631d88a3467c11222c
sirvan monfared <sirvan.monfared.dev@gmail.com>
2024-03-20 15:01:03 +0330

    s08e10

--- index.php
+++ index.php
@@ -1,34 +1,9 @@
 <?php
-declare(strict_types=1);
+function sum(string $text, int|float ...$numbers) {
 
-function sayMyName(int $text): ?string
-{
-    $user = true;
-    if (! $user) {
-        return null;
-    }
+    return $text . ' ' . array_sum($numbers);
 
-    return "<strong>{$text}</strong>";
 }
 
+echo sum('Total Sum of these numbers are:', 3, 5, 8, 10, 20);
 
-echo sayMyName(32532);
-
-// function sum(int $x,int $y) {
-
-//     return $x + $y;
-// }
-
-// // var_dump(sum(2.1, 3));
-
-// class Math {
-//     public int $total;
-
-//     public function sum(int $x,int $y) {
-        
-//         $this->total = 9.2;
-//         return $this->total;
-//     }
-// }
-
-// echo (new Math)->sum(2, 3);
