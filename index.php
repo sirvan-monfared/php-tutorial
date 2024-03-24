95f05ea34480129443d38c712e927196881f6345
sirvan monfared <sirvan.monfared.dev@gmail.com>
2024-03-24 17:24:35 +0330

    s09e08

--- index.php
+++ index.php
@@ -3,20 +3,27 @@
 
 // 1. Encapsulation
 // 2. Abstraction
-
-
-
-
 // 3. Inheritance
-require('Car.php');
-require('Dena.php');
-require('BMW.php');
-$dena = new Dena();
-$dena->move(20, 100);
 
-
-// $bmw = new BMW();
 
 
 
 // 4. Polymorphism
+require('Car.php');
+require('Dena.php');
+require('BMW.php');
+
+
+class User {
+
+    public $car;
+
+    public function __construct(Car $car) {
+        $this->car = $car;
+    }
+
+}
+
+$user = new User(new Dena);
+
+$user->car->move(210, 50);
