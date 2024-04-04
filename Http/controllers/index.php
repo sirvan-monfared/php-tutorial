6d7c0b0957c8d386b6b46baaff95d6aff8d37c80
sirvan monfared <sirvan.monfared.dev@gmail.com>
2024-04-04 16:57:45 +0330

    s11e03

--- Http/controllers/index.php
+++ Http/controllers/index.php
@@ -1,10 +1,27 @@
 <?php
+
+use Illuminate\Support\Collection;
+use Carbon\Carbon;
+
+
 $_SESSION['name'] = 'sirvan';
 
 $db = new Core\Database();
 $posts = $db->prepare("SELECT * FROM `posts`")->all();
 
 $name = 'sirvan';
+
+
+// $data = [1,2,3,4,5,6,7,8,9];
+// $newArray = (new Collection($data))->filter(fn($number) => $number <= 6);
+// dd($newArray->split(2)->first()->first());
+
+
+// $date = Carbon::now();
+// $newDate = $date->subMonths(4)->addHours(5);
+// var_dump($newDate->diffForHumans());
+// dd($newDate->format('Y/m/d H:i:s'));
+// dd($date->subMonths(4)->addHours(5)->diffForHumans());
 
 view('index.view.php', [
     'title' => 'Home',
--- public/index.php
+++ public/index.php
@@ -2,20 +2,14 @@
 
 use Core\Session;
 
-session_start();
-
 const BASE_PATH = __DIR__ . '/../';
+
+require BASE_PATH . "vendor/autoload.php";
+
+session_start();
 
 require BASE_PATH . "functions.php";
 
-spl_autoload_register(function($class) {
-
-    $class_name = str_replace('\\', '/', $class);
-
-    require_once(base_path("{$class_name}.php"));
-});
-
-// require base_path('Core/router.php'); 
 
 $url = currentUrl();
 
