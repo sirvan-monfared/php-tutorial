13bc4971fae687c14c5a07d4811d8a2f8c1bdd4f
sirvan monfared <sirvan.monfared.dev@gmail.com>
2024-04-11 21:14:13 +0330

    s11e12

--- views/index.blade.php
+++ views/index.blade.php
@@ -1,16 +1,17 @@
 @extends('layouts.main')
 
-
 @section('content')
     <main>
         <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
             <section class="grid grid-cols-3 gap-2">
-            <?php foreach($posts as $post): ?>
-                <article class="bg-white shadow-md p-5 flex flex-col gap-2">
-                    <img src="https://placehold.co/600x400" alt="">
-                    <p><a href="<?= route('posts.show', ['id' => $post->id]) ?>"><?= $post->title ?></a></p>
-                </article>
-            <?php endforeach; ?>
+
+                @foreach($posts as $post)
+                    <article class="bg-white shadow-md p-5 flex flex-col gap-2">
+                        <img src="https://placehold.co/600x400" alt="">
+                        <p><a href="{{ route('posts.show', ['id' => $post->id]) }}">{{ $post->title }}</a></p>
+                    </article>
+                @endforeach
+
             </section>
         </div>
     </main>
---
+++ views/notes/edit.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.main', [
+    'pageTitle' => 'Edit Note',
+])
+
+@section('content')
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <form method="POST" action="<?= route('notes.update', ['id' => $note['id']]) ?>">
+
+            <input type="hidden" name="_method" value="PATCH">
+
+            <div class="space-y-12">
+                <div class="border-b border-gray-900/10 pb-12">
+                    <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                        <div class="sm:col-span-4">
+                            <label for="title" class="block text-sm font-medium leading-6 text-gray-900">title</label>
+                            <div class="mt-2">
+                                <div
+                                    class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                    <input type="text" name="title" id="title" value="<?= $note['title'] ?>"
+                                        class="bg-white block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6">
+                                </div>
+                                <p class="text-red-500 text-xs">{{ $errors['title'] ?? null }}</p>
+
+                            </div>
+                        </div>
+
+                        <div class="col-span-full">
+                            <label for="body" class="block text-sm font-medium leading-6 text-gray-900">body</label>
+                            <div class="mt-2">
+                                <textarea id="body" name="body" rows="3"
+                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"><?= $note['body'] ?></textarea>
+                            </div>
+                            <p class="text-red-500 text-xs">{{ $errors['body'] ?? null }}</p>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+
+            <div class="mt-6 flex items-center justify-end gap-x-6">
+                <a class="bg-gray-500 text-white px-3 py-2 rounded-md text-sm"
+                    href="/notes/show?id=<?= $note['id'] ?>">Cancel</a>
+                <button type="submit"
+                    class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Update</button>
+            </div>
+        </form>
+    </div>
+@endsection
